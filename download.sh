37f40ccb6f7d04f62676241e25ae24e9626d4aba
Nik Volynkin <niknorff@gmail.com>
2023-03-03 17:13:28 +0200

    Modifying ingest and download scripts, generating vector store

--- download.sh
+++ download.sh
@@ -2,5 +2,5 @@
 # This involves scraping the data from the web and then cleaning up and putting in Weaviate.
 # Error if any command fails
 set -e
-echo Downloading docs...
-wget -q -r -A.html https://langchain.readthedocs.io/en/latest/
+echo Downloading book...
+wget -q -r -A.htm https://gutenberg.org/files/20203/20203-h/20203-h.htm
--- ingest.ts
+++ ingest.ts
@@ -8,77 +8,77 @@ import path from "path";
 import { load } from "cheerio";
 
 async function processFile(filePath: string): Promise<Document> {
-  return await new Promise<Document>((resolve, reject) => {
-    fs.readFile(filePath, "utf8", (err, fileContents) => {
-      if (err) {
-        reject(err);
-      } else {
-        const text = load(fileContents).text();
-        const metadata = { source: filePath };
-        const doc = new Document({ pageContent: text, metadata: metadata });
-        resolve(doc);
-      }
-    });
-  });
+	return await new Promise<Document>((resolve, reject) => {
+		fs.readFile(filePath, "utf8", (err, fileContents) => {
+			if (err) {
+				reject(err);
+			} else {
+				const text = load(fileContents).text();
+				const metadata = { source: filePath };
+				const doc = new Document({ pageContent: text, metadata: metadata });
+				resolve(doc);
+			}
+		});
+	});
 }
 
 async function processDirectory(directoryPath: string): Promise<Document[]> {
-  const docs: Document[] = [];
-  let files: string[];
-  try {
-    files = fs.readdirSync(directoryPath);
-  } catch (err) {
-    console.error(err);
-    throw new Error(
-      `Could not read directory: ${directoryPath}. Did you run \`sh download.sh\`?`
-    );
-  }
-  for (const file of files) {
-    const filePath = path.join(directoryPath, file);
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      const newDocs = processDirectory(filePath);
-      const nestedDocs = await newDocs;
-      docs.push(...nestedDocs);
-    } else {
-      const newDoc = processFile(filePath);
-      const doc = await newDoc;
-      docs.push(doc);
-    }
-  }
-  return docs;
+	const docs: Document[] = [];
+	let files: string[];
+	try {
+		files = fs.readdirSync(directoryPath);
+	} catch (err) {
+		console.error(err);
+		throw new Error(
+			`Could not read directory: ${directoryPath}. Did you run \`sh download.sh\`?`
+		);
+	}
+	for (const file of files) {
+		const filePath = path.join(directoryPath, file);
+		const stat = fs.statSync(filePath);
+		if (stat.isDirectory()) {
+			const newDocs = processDirectory(filePath);
+			const nestedDocs = await newDocs;
+			docs.push(...nestedDocs);
+		} else {
+			const newDoc = processFile(filePath);
+			const doc = await newDoc;
+			docs.push(doc);
+		}
+	}
+	return docs;
 }
 
 class ReadTheDocsLoader extends BaseDocumentLoader {
-  constructor(public filePath: string) {
-    super();
-  }
-  async load(): Promise<Document[]> {
-    return await processDirectory(this.filePath);
-  }
+	constructor(public filePath: string) {
+		super();
+	}
+	async load(): Promise<Document[]> {
+		return await processDirectory(this.filePath);
+	}
 }
 
-const directoryPath = "langchain.readthedocs.io";
+const directoryPath = "gutenberg.org";
 const loader = new ReadTheDocsLoader(directoryPath);
 
 export const run = async () => {
-  const rawDocs = await loader.load();
-  console.log("Loader created.");
-  /* Split the text into chunks */
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 200,
-  });
-  const docs = await textSplitter.splitDocuments(rawDocs);
-  console.log("Docs splitted.");
+	const rawDocs = await loader.load();
+	console.log("Loader created.");
+	/* Split the text into chunks */
+	const textSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 1000,
+		chunkOverlap: 200,
+	});
+	const docs = await textSplitter.splitDocuments(rawDocs);
+	console.log("Docs splitted.");
 
-  console.log("Creating vector store...");
-  /* Create the vectorstore */
-  const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-  await vectorStore.save("data");
+	console.log("Creating vector store...");
+	/* Create the vectorstore */
+	const vectorStore = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+	await vectorStore.save("data");
 };
 
 (async () => {
-  await run();
-  console.log("done");
+	await run();
+	console.log("done");
 })();
